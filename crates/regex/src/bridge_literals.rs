78d04bbea29b5a66247ad7ec54757cb2ec8db01e
Ionuț Nicula <nicula@nicula.xyz>
2026-02-06 00:07:07 +0000

    Literal extraction improvements + tests
    Improvements:
    
    * extract literals from alternations by taking the common prefixes and
      suffixes out of the alternation itself;
    * use the minimum required length from the HIR if it's bigger than the
      one this new approach calculates;

--- crates/regex/src/bridge_literals.rs
+++ crates/regex/src/bridge_literals.rs
@@ -3,13 +3,13 @@ use regex_syntax::hir::{self, Hir, HirKind};
 
 /// A sequence of literals that must appear in a specific order for a line to qualify as a
 /// candidate line.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct LiteralSequence {
     seq: Vec<Vec<u8>>,
     min_required_len: usize,
 }
 
-#[derive(Copy, Clone, Debug)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq)]
 enum LiteralComponent {
     Char(u8),
     Break,
@@ -18,6 +18,15 @@ enum LiteralComponent {
 impl LiteralSequence {
     /// Constructs a new `LiteralSequence` from a `Hir`.
     pub fn new(hir: &Hir) -> Option<LiteralSequence> {
+        let mut result = Self::from_hir(hir);
+        result.min_required_len = std::cmp::max(
+            result.min_required_len,
+            hir.properties().minimum_len().unwrap_or(0),
+        );
+        if result.is_useful() { Some(result) } else { None }
+    }
+
+    fn from_hir(hir: &Hir) -> LiteralSequence {
         let components = extract_literal_seq_components(hir);
 
         let mut result = vec![vec![]];
@@ -47,8 +56,7 @@ impl LiteralSequence {
             result.pop();
         }
 
-        let lseq = LiteralSequence { seq: result, min_required_len: len };
-        if lseq.is_useful() { Some(lseq) } else { None }
+        LiteralSequence { seq: result, min_required_len: len }
     }
 
     /// Checks if the literal sequence exists in `haystack`.
@@ -62,7 +70,7 @@ impl LiteralSequence {
         if haystack.is_empty() {
             return None;
         }
-        if self.min_required_len == 0 {
+        if self.seq.is_empty() {
             return Some(0);
         }
 
@@ -84,7 +92,7 @@ impl LiteralSequence {
     /// Heuristic for whether using the literal sequence will provide performance improvements, or
     /// at least not significantly reduce the performance.
     fn is_useful(self: &LiteralSequence) -> bool {
-        self.seq.len() >= 2
+        self.seq.len() >= 2 && self.min_required_len >= 8
     }
 }
 
@@ -99,14 +107,42 @@ fn extract_literal_seq_components(hir: &Hir) -> Vec<LiteralComponent> {
         HirKind::Concat(sub_hirs) => {
             sub_hirs.iter().flat_map(extract_literal_seq_components).collect()
         }
-        HirKind::Alternation(_) => vec![LiteralComponent::Break],
+        HirKind::Alternation(sub_hirs) => {
+            let sub_results: Vec<Vec<LiteralComponent>> =
+                sub_hirs.iter().map(extract_literal_seq_components).collect();
+
+            // An alternation like "(axc)|(ayc)", for example, is equivalent to "a(x|y)c". Based on
+            // this idea we extract the common prefix and the common suffix as literal components
+            // *outside* of the alternation, which allows us to accumulate more literals.
+            let (mut left, right) =
+                get_common_prefix_and_suffix(sub_results.as_slice());
+
+            let max_len =
+                sub_results.iter().map(|r| r.len()).max().unwrap_or(0);
+            // Only insert a break character if at least one of the alternatives is different from
+            // the others. An expression like "(abc|abc)", for example, is equivalent to "abc", a
+            // literal.
+            // This allows us to avoid inserting unnecessary break characters, thus allowing more
+            // literals to be extracted.
+            if left.len() != max_len {
+                push_without_consecutive_break(
+                    &mut left,
+                    LiteralComponent::Break,
+                );
+                append_without_consecutive_break(&mut left, &right);
+            }
+
+            left
+        }
         HirKind::Class(_) => vec![LiteralComponent::Break],
         HirKind::Repetition(rep) => {
             let mut result = if rep.min == 0 {
                 vec![]
             } else {
-                extract_literal_seq_components(&rep.sub)
-                    .repeat(rep.min as usize)
+                repeat_without_consecutive_break(
+                    &extract_literal_seq_components(&rep.sub),
+                    rep.min as usize,
+                )
             };
 
             // If `rep.max` is strictly greater than `rep.min`, then after repeating the literals
@@ -119,10 +155,365 @@ fn extract_literal_seq_components(hir: &Hir) -> Vec<LiteralComponent> {
             // If we don't do this, then expressions like "ab*c" would have the required literals
             // ["ac"], which is incorrect. The correct literals in this case are: ["a", "c"].
             if rep.max.unwrap_or(u32::MAX) > rep.min {
-                result.push(LiteralComponent::Break);
+                push_without_consecutive_break(
+                    &mut result,
+                    LiteralComponent::Break,
+                );
             }
 
             result
         }
+    }
+}
+
+fn push_without_consecutive_break(
+    vec: &mut Vec<LiteralComponent>,
+    c: LiteralComponent,
+) {
+    if !(c == LiteralComponent::Break
+        && vec.last() == Some(&LiteralComponent::Break))
+    {
+        vec.push(c)
+    }
+}
+
+fn append_without_consecutive_break(
+    vec: &mut Vec<LiteralComponent>,
+    other: &Vec<LiteralComponent>,
+) {
+    for &c in other {
+        push_without_consecutive_break(vec, c);
+    }
+}
+
+fn repeat_without_consecutive_break(
+    vec: &Vec<LiteralComponent>,
+    times: usize,
+) -> Vec<LiteralComponent> {
+    let mut result = Vec::new();
+    result.reserve_exact(times * vec.len());
+
+    for _ in 0..times {
+        for &c in vec {
+            push_without_consecutive_break(&mut result, c);
+        }
+    }
+
+    result
+}
+
+fn get_common_prefix_and_suffix(
+    seqs: &[Vec<LiteralComponent>],
+) -> (Vec<LiteralComponent>, Vec<LiteralComponent>) {
+    if seqs.is_empty() {
+        return (vec![], vec![]);
+    }
+
+    let left: Vec<LiteralComponent> = seqs[0]
+        .iter()
+        .copied()
+        .enumerate()
+        .take_while(|&(i, c)| seqs.iter().all(|seq| seq.get(i) == Some(&c)))
+        .map(|(_, c)| c)
+        .collect();
+
+    let mut right: Vec<LiteralComponent> = seqs[0]
+        .iter()
+        .copied()
+        .skip(left.len())
+        .rev()
+        .enumerate()
+        .take_while(|&(i, c)| {
+            seqs.iter().all(|seq| seq.iter().rev().nth(i) == Some(&c))
+        })
+        .map(|(_, c)| c)
+        .collect();
+    right.reverse();
+
+    (left, right)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use regex_syntax::hir::Repetition;
+
+    #[test]
+    fn extract_literals1() {
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::literal("abc".as_bytes())),
+            LiteralSequence { seq: vec!["abc".into()], min_required_len: 3 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 0,
+                max: None,
+                sub: Box::new(Hir::literal("abcde".as_bytes())),
+                greedy: false,
+            })),
+            LiteralSequence { seq: vec![], min_required_len: 0 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 1,
+                max: None,
+                sub: Box::new(Hir::literal("abcde".as_bytes())),
+                greedy: false,
+            })),
+            LiteralSequence { seq: vec!["abcde".into()], min_required_len: 5 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 5,
+                max: Some(5),
+                sub: Box::new(Hir::literal("abcde".as_bytes())),
+                greedy: false,
+            })),
+            LiteralSequence {
+                seq: vec!["abcdeabcdeabcdeabcdeabcde".into()],
+                min_required_len: 25
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 5,
+                max: Some(10),
+                sub: Box::new(Hir::literal("abcde".as_bytes())),
+                greedy: false,
+            })),
+            LiteralSequence {
+                seq: vec!["abcdeabcdeabcdeabcdeabcde".into()],
+                min_required_len: 25
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 5,
+                max: None,
+                sub: Box::new(Hir::literal("abcde".as_bytes())),
+                greedy: false,
+            })),
+            LiteralSequence {
+                seq: vec!["abcdeabcdeabcdeabcdeabcde".into()],
+                min_required_len: 25
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![Hir::literal(
+                "abc".as_bytes()
+            ),])),
+            LiteralSequence { seq: vec!["abc".into()], min_required_len: 3 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("abc".as_bytes()),
+                Hir::literal("abc".as_bytes()),
+                Hir::literal("abc".as_bytes()),
+            ])),
+            LiteralSequence { seq: vec!["abc".into()], min_required_len: 3 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("abcd".as_bytes()),
+                Hir::literal("abc".as_bytes()),
+                Hir::literal("ab".as_bytes()),
+            ])),
+            LiteralSequence { seq: vec!["ab".into()], min_required_len: 2 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("abcd".as_bytes()),
+                Hir::literal("bcd".as_bytes()),
+                Hir::literal("cd".as_bytes()),
+            ])),
+            LiteralSequence { seq: vec!["cd".into()], min_required_len: 2 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("abcd".as_bytes()),
+                Hir::literal("bcd".as_bytes()),
+                Hir::literal("cd".as_bytes()),
+                Hir::literal("c".as_bytes()),
+                Hir::literal("".as_bytes()),
+            ])),
+            LiteralSequence { seq: vec![], min_required_len: 0 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("abc".as_bytes()),
+                Hir::literal("axc".as_bytes()),
+            ])),
+            LiteralSequence {
+                seq: vec!["a".into(), "c".into()],
+                min_required_len: 2,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("abc".as_bytes()),
+                Hir::literal("axc".as_bytes()),
+                Hir::literal("axd".as_bytes()),
+            ])),
+            LiteralSequence { seq: vec!["a".into()], min_required_len: 1 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("abc".as_bytes()),
+                Hir::literal("axc".as_bytes()),
+                Hir::literal("vxd".as_bytes()),
+            ])),
+            LiteralSequence { seq: vec![], min_required_len: 0 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::alternation(vec![
+                Hir::literal("how".as_bytes()),
+                Hir::literal("cow".as_bytes()),
+                Hir::literal("meow".as_bytes()),
+            ])),
+            LiteralSequence { seq: vec!["ow".into()], min_required_len: 2 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::concat(vec![
+                Hir::literal("how".as_bytes()),
+                Hir::literal("cow".as_bytes()),
+                Hir::literal("meow".as_bytes()),
+            ])),
+            LiteralSequence {
+                seq: vec!["howcowmeow".into()],
+                min_required_len: 10,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::concat(vec![
+                Hir::literal("hello".as_bytes()),
+                Hir::alternation(vec![
+                    Hir::literal("how".as_bytes()),
+                    Hir::literal("cow".as_bytes()),
+                    Hir::literal("meow".as_bytes()),
+                ])
+            ])),
+            LiteralSequence {
+                seq: vec!["hello".into(), "ow".into()],
+                min_required_len: 7,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::concat(vec![
+                Hir::literal("hello".as_bytes()),
+                Hir::alternation(vec![
+                    Hir::literal("view".as_bytes()),
+                    Hir::literal("vinyl".as_bytes()),
+                    Hir::literal("video".as_bytes()),
+                ])
+            ])),
+            LiteralSequence {
+                seq: vec!["hellovi".into()],
+                min_required_len: 7,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::concat(vec![
+                Hir::literal("hello".as_bytes()),
+                Hir::alternation(vec![
+                    Hir::literal("aiew".as_bytes()),
+                    Hir::literal("binyl".as_bytes()),
+                    Hir::literal("cideo".as_bytes()),
+                ])
+            ])),
+            LiteralSequence { seq: vec!["hello".into()], min_required_len: 5 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::concat(vec![
+                Hir::literal("hello".as_bytes()),
+                Hir::alternation(vec![
+                    Hir::literal("aiyx".as_bytes()),
+                    Hir::literal("ainyx".as_bytes()),
+                    Hir::literal("aidyx".as_bytes()),
+                ])
+            ])),
+            LiteralSequence {
+                seq: vec!["helloai".into(), "yx".into()],
+                min_required_len: 9,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 5,
+                max: Some(5),
+                sub: Box::new(Hir::alternation(vec![
+                    Hir::literal("abc".as_bytes()),
+                    Hir::literal("def".as_bytes()),
+                ])),
+                greedy: false,
+            })),
+            LiteralSequence { seq: vec![], min_required_len: 0 }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 3,
+                max: Some(5),
+                sub: Box::new(Hir::alternation(vec![
+                    Hir::literal("abc".as_bytes()),
+                    Hir::literal("axc".as_bytes()),
+                ])),
+                greedy: false,
+            })),
+            LiteralSequence {
+                seq: vec!["a".into(), "ca".into(), "ca".into(), "c".into()],
+                min_required_len: 6,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::concat(vec![
+                Hir::literal("x".as_bytes()),
+                Hir::alternation(vec![
+                    Hir::literal("ab".as_bytes()),
+                    Hir::literal("b".as_bytes()),
+                ]),
+                Hir::literal("y".as_bytes()),
+            ])),
+            LiteralSequence {
+                seq: vec!["x".into(), "by".into()],
+                min_required_len: 3,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 3,
+                max: Some(5),
+                sub: Box::new(Hir::concat(vec![
+                    Hir::literal("x".as_bytes()),
+                    Hir::alternation(vec![
+                        Hir::literal("ab".as_bytes()),
+                        Hir::literal("b".as_bytes()),
+                    ]),
+                    Hir::literal("y".as_bytes()),
+                ])),
+                greedy: false,
+            })),
+            LiteralSequence {
+                seq: vec!["x".into(), "byx".into(), "byx".into(), "by".into()],
+                min_required_len: 9,
+            }
+        );
+        assert_eq!(
+            LiteralSequence::from_hir(&Hir::repetition(Repetition {
+                min: 3,
+                max: Some(5),
+                sub: Box::new(Hir::concat(vec![
+                    Hir::literal("x".as_bytes()),
+                    Hir::alternation(vec![
+                        Hir::literal("ab".as_bytes()),
+                        Hir::literal("a".as_bytes()),
+                    ]),
+                    Hir::literal("y".as_bytes()),
+                ])),
+                greedy: false,
+            })),
+            LiteralSequence {
+                seq: vec!["xa".into(), "yxa".into(), "yxa".into(), "y".into()],
+                min_required_len: 9,
+            }
+        );
     }
 }
